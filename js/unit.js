794484de5145e6e348484485fab58a474eb2d5f9
DEV SK GAMER <82174893+Devsk001@users.noreply.github.com>
2023-05-05 08:32:18 +0530

    initial changes video container added
    video and video container and some css changed responsive

--- js/unit.js
+++ js/unit.js
@@ -1,1 +1,32 @@
-console.log("smple test");
+console.log(" Welcome to UcerIdiotz");
+
+const videoText = document.getElementById("video-text");
+const videoIframeContainer = document.getElementById("video-iframe-container");
+const topics=document.getElementById("unit-topics");
+const unit_div=document.getElementById('unit-div');
+const english=document.getElementById('english');
+const video_english=document.getElementById('video-iframe-english');
+const video_tamil=document.getElementById('video-iframe');
+
+videoText.addEventListener("click", function() {
+  if (videoIframeContainer.style.display === "none") {
+    videoIframeContainer.style.display = "block";
+    unit_div.classList.add("unit-div-2");
+  } else {
+    videoIframeContainer.style.display = "none";
+    unit_div.classList.remove("unit-div-2");
+  }
+});
+
+
+english.addEventListener("click", function() {
+  if (video_english.style.display === "none") {
+    video_english.style.display = "block";
+     video_tamil.style.display="none";
+     english.innerHTML="For Tamil"
+  } else {
+    video_english.style.display = "none";
+    video_tamil.style.display="block";
+    english.innerHTML="For English"
+  }
+});
