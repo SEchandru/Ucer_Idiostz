d6b9ef019e7eedc08f0e842189be0ef8ee5dfea3
DEV SK GAMER <82174893+Devsk001@users.noreply.github.com>
2023-05-05 09:48:41 +0530

    Updated the second text js

--- js/unit.js
+++ js/unit.js
@@ -1,6 +1,8 @@
 console.log(" Welcome to UcerIdiotz");
 
 const videoText = document.getElementById("video-text");
+
+const videoText01 = document.getElementById("video-text");
 const videoIframeContainer = document.getElementById("video-iframe-container");
 const topics=document.getElementById("unit-topics");
 const unit_div=document.getElementById('unit-div');
@@ -18,6 +20,16 @@ videoText.addEventListener("click", function() {
   }
 });
 
+
+videoText01.addEventListener("click", function() {
+  if (videoIframeContainer.style.display === "none") {
+    videoIframeContainer.style.display = "block";
+    unit_div.classList.add("unit-div-2");
+  } else {
+    videoIframeContainer.style.display = "none";
+    unit_div.classList.remove("unit-div-2");
+  }
+});
 
 english.addEventListener("click", function() {
   if (video_english.style.display === "none") {
